428d0d7ff9fc1f44ab3d7350f2b935b65b9415b9
dirigeants <dirigeants.inc@gmail.com>
2026-03-06 02:21:10 +0200

    refactor(controller): improve type annotations & import paths in team-controller

--- src/controllers/team-controller.js
+++ src/controllers/team-controller.js
@@ -1,16 +1,23 @@
-import { teamData } from '../data/team-data.js';
+import { teamData } from '#data/team-data.js';
 
-// @ts-ignore
-export const getAllMembers = (req, res) => {
+/**
+ * @param {import('express').Request} _req
+ * @param {import('express').Response} res
+ */
+export const getAllMembers = (_req, res) => {
 	res.render('team', { members: teamData });
 };
 
-// @ts-ignore
-// eslint-disable-next-line consistent-return
+/**
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ */
 export const getMemberById = (req, res) => {
 	const member = teamData.find((m) => m.id === Number.parseInt(req.params.id));
 	if (!member) {
-		return res.status(404).send('Member not found');
+		res.status(404).send('Member not found');
+		return;
 	}
+
 	res.render('member', { member });
 };
